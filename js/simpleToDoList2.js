faef45aab3103244b00719f42472bf1ba7d2a4a9
Igor Kozlowski <igor3648@gmail.com>
2022-10-11 18:58:22 +0200

    list items removed

--- js/simpleToDoList2.js
+++ js/simpleToDoList2.js
@@ -1,14 +1,5 @@
 {
-    const tasks = [
-        {
-            content: "nagrać lekcję",
-            done: false,
-        },
-        {
-            content: "zjeść",
-            done: true,
-        },
-    ]
+    const tasks = []
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
@@ -52,17 +43,17 @@
         for (const task of tasks) {
             htmlString += `
             
-        <li class="list__item">
-            <button class="list__button list__button--done js-done">
-                ${task.done ? "✔" : ""}
-            </button>
-            <span class="${task.done ? "list__item--done" : ""}">
-                ${task.content}
-            </span>
-            <button class="list__button list__button--remove js-remove">
-            🗑
-            </button>
-        </li>
+                <li class="list__item">
+                    <button class="list__button list__button--done js-done">
+                        ${task.done ? "✔" : ""}
+                    </button>
+                    <span class="${task.done ? "list__item--done" : ""}">
+                        ${task.content}
+                    </span>
+                    <button class="list__button list__button--remove js-remove">
+                    🗑
+                    </button>
+                </li>
             `;
         };
 
